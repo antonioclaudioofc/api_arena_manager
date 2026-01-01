34c24187806e32e71e537fe8a160b0f06494bb44
Antonio Claudio <claudioalvesdev@gmail.com>
2026-01-01 12:51:59 -0300

    fix: bug github

--- app/core/database.py
+++ app/core/database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from core.config import DATABASE_URL
+from app.core.config import DATABASE_URL
 
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
